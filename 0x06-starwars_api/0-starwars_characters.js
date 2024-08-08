ffb59f692e8589992fa5bfa4059b3717673ea7cd
kiddingmz <bata.isidro@gmail.com>
2024-08-08 22:21:02 +0200

    CHORE: 0. Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,30 @@
 #!/usr/bin/node
+
 const request = require('request');
+const FILMID = process.argv[2];
 
-const movieId = process.argv[2];
+const URL_BASE = 'https://swapi-api.alx-tools.com/api/films';
 
-request(
-  `https://swapi-api.alx-tools.com/api/films/${movieId}/`,
-  async (error, response, body) => {
-    if (error) {
-      console.log(error);
-    }
-    const characters = JSON.parse(body).characters;
-    for (const characterUrl of characters) {
-      await new Promise((resolve, reject) => {
-        request(characterUrl, (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else {
-            console.log(JSON.parse(body).name);
-            resolve();
-          }
-        });
-      });
-    }
+function doRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, res, body) {
+      if (!error && res.statusCode === 200) {
+        resolve(JSON.parse(body));
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+// Usage:
+
+async function main (filmID) {
+  const res = await doRequest(`${URL_BASE}/${filmID}`);
+  for (const e of res.characters) {
+    const pj = await doRequest(e);
+    console.log(pj.name);
   }
-);
+}
+
+main(FILMID);
